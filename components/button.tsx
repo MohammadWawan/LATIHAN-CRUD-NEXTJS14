898376c8c8953469b2efd060e3d5e097051f954b
Mohammad Wawan <40724858+MohammadWawan@users.noreply.github.com>
2024-10-04 16:58:05 +0700

    Update button.tsx

--- components/button.tsx
+++ components/button.tsx
@@ -32,7 +32,7 @@ export const EditButton = ({ id }: { id: string }) => {
 export const DeleteButton = ({ id }: { id: string }) => {
   const DeleteContactWithId = deleteContact.bind(null, id);
   return (
-    <form action={DeleteContactWithId as any}>
+    <form action={DeleteContactWithId}>
       <button className="rounded-sm border p-1 hover:bg-gray-100">
         <IoTrashOutline size={20} />
       </button>
